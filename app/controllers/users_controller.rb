f6e95b01872f45a2a49ade890f9c349a9974be14
meltrust <original.mtapia@outlook.com>
2021-03-25 13:25:40 -0600

    push to partner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
-  def index
-  end
-  
+  def index; end
+
   def new
-    
+    #  @user = User.new
   end
 
   def create
-  
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 end
