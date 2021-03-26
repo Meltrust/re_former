a356db3196dbf07fc5870245a14e4deb957bb504
Chukwuma Obasi <ccobasi8@gmail.com>
2021-03-26 22:00:12 +0100

    Add edit content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # resources :users, only: [:new, :create]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit]
   # root 'users#index'
   # resources :users
     # root 'users#new'
