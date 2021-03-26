d73e7b47a17b648a7ae2be2e04a21a0dcee63cc5
Chukwuma Obasi <ccobasi8@gmail.com>
2021-03-26 15:50:48 +0100

    Add form shanges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index; end
 
   def new
-    #  @user = User.new
+     @user = User.new
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   # root 'users#index'
   # resources :users
-  #   root 'users#new'
+    # root 'users#new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
